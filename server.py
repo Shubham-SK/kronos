0bfe2ad66080d703576209555553ce40a35609b1
Pranav Srisankar <pranavsri623@gmail.com>
2019-09-21 16:05:59 -0700

    doin time 2

--- server.py
+++ server.py
@@ -2,15 +2,22 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/button')
-def button():
+@app.route('/start')
+def start():
     print("Hello World")
-    return render_template("STOP.html")
+    return render_template("START.html")
     
+
+@app.route('/stop')
+def stop():
+    return render_template("STOP.html")
 
 @app.route('/')
 def index():
-    return render_template("START.html")
+    return render_template("START.html")    
+
+  
+
 
 if __name__ == "__main__":
     app.run(debug=True)
