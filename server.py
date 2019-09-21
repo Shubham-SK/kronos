a26c2812477d33c222d7b81741502ff7b568e90d
Pranav Srisankar <pranavsri623@gmail.com>
2019-09-21 15:18:18 -0700

    bob

--- server.py
+++ server.py
@@ -1,35 +1,16 @@
 from flask import Flask, request, render_template
 
-main_page = """
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>Button</h2>
-<form action="button">
-    <button type="submit">Press Button!</button>
-<form>
- 
-</body>
-</html>
-"""
-
 app = Flask(__name__)
 
 @app.route('/button')
 def button():
     print("Hello World")
-    return main_page
+    return render_template("STOP.html")
     
 
 @app.route('/')
 def index():
-    return render_template("home.html")
+    return render_template("START.html")
 
-<<<<<<< HEAD
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> f9585d9827fba98d583e933dc194fe2f1329c7bc
