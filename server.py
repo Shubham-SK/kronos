3b3e40d74979e16829980b505a2e0a3155886806
Shubham Kumar <kumar.shubham5504@gmail.com>
2019-09-21 15:17:52 -0700

    head removed

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ main_page = """
 <form action="button">
     <button type="submit">Press Button!</button>
 <form>
- 
+
 </body>
 </html>
 """
@@ -20,16 +20,10 @@ app = Flask(__name__)
 def button():
     print("Hello World")
     return main_page
-    
 
 @app.route('/')
 def index():
     return render_template("home.html")
 
-<<<<<<< HEAD
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> f9585d9827fba98d583e933dc194fe2f1329c7bc
