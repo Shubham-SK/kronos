069bb656e4faf2e5d1aa128d2ef4efa35fcdab73
Shubham Kumar <kumar.shubham5504@gmail.com>
2019-09-22 04:11:16 -0700

    fixed audio playback of doctor conversation + HTML

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import os, subprocess, time
 
 app = Flask(__name__)
@@ -13,16 +13,20 @@ def stop():
 def start():
     os.system("python stopublish.py")
     return render_template("START.html")
-    
 
+@app.route('/blood-pres')
+def blood_pres():
+    return redirect("http://io.adafruit.com/vishakh_arora29/feeds/blood-pressure")
 
+@app.route('/heart-rate')
+def heart_rate():
+    return redirect("http://io.adafruit.com/vishakh_arora29/feeds/heart-rate")
 
 @app.route('/')
 def index():
+    return render_template("START.html")
 
-    return render_template("START.html")    
 
-  
 
 
 if __name__ == "__main__":
