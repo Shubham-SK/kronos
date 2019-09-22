0120af80376eb3d27867e7f1287fb99bff05fc23
tiger135 <vishakh.arora29@gmail.com>
2019-09-21 23:15:03 -0700

    working MQTT from command line, but doesn't work on Flask

--- mqtt_computer.py
+++ mqtt_computer.py
@@ -1,13 +1,12 @@
-import sys
+import sys, os
 import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
 import time
 # try:
 #     import RPi.GPIO as GPIO
 # except RuntimeError:
 #     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-TOPIC_PATH="/omnihacks/record/9"
+TOPIC_PATH="/omnihacks/record/10"
 OFF=0
 ON=1
 CHANNELS = [3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26,29,31,32,33,35,36,37]
@@ -34,15 +33,28 @@ def on_connect(client, userdata, flags, rc):
 #     return GPIO.LOW
 
 # The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("received")
+    f = open('new_doctor.wav', 'wb')
+    f.write(msg.payload)
+    f.close()
+    os.system("afplay new_doctor.wav")
 
 if len(sys.argv) > 1 and sys.argv[1] == '-d':
     DEBUG = True
 client = mqtt.Client()
 client.on_connect = on_connect
-#client.on_message = on_message
+client.on_message = on_message
 
 print("connecting")
 client.connect("10.1.221.36")
 print("connected")
-
-client.publish('/omnihacks/record/9', 'start')
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+# GPIO.setup(CHANNELS, GPIO.OUT)
+# GPIO.output(CHANNELS, gpioStateFor(OFF))
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
