66f02baa16d820b19162525b345e288e11f3348a
shlokmehrotra <shlokmehrotra@gmail.com>
2019-09-21 22:00:12 -0700

    Update transcript_create.py

--- Transcript-Generation/transcript_create.py
+++ Transcript-Generation/transcript_create.py
@@ -1,3 +1,4 @@
+
 #Array of words -> word, num
 convarr = [["I'm", 1], ['prescribing', 1], ['10', 1], ['mg', 1], ['of', 1], ['alendronate', 1], ['and', 1], ['5', 1], ['mg', 1], ['of', 1], ['acetaminophen', 1], ['excellent', 3], ['that', 3], ['should', 3], ['work', 3]]
 final = []
@@ -18,15 +19,23 @@ for i in range(len(convarr)):
 		string += convarr[i][0]
 	if(i != 0 and convarr[i][1] != convarr[i-1][1]):
 		final.append(string)
-		string = ""
+		string = ""	
+log = []
+
 final.append(string)
 with open("transcript.txt", 'w') as f:
 	f.write('TRANSCRIPT')
 	f.write('\n___________')
 	for i in range(len(final)):
+		if(final[i].split(' ')[0] == "log"):
+			log.append(final[i])
 		if(i % 2 == 0 ):
 			final[i] = "\nDoctor: " + final[i]
 			f.write(final[i])
 		if(i % 2 == 1):
 			final[i] = "\nPatient: " + final[i]
 			f.write(final[i])
+	f.write('\nLOG')
+	f.write('\n___________')
+	for i in range(len(log)):
+		f.write(log[i])
